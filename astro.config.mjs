ceef8cc2c10b28af1ab259e3ae4d13d54074281a
RickCrispin <74029241@continental.edu.pe>
2026-04-28 12:28:58 -0500

    Simplify config - use Vercel auto-detection for static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    },
-    imagesConfig: {
-      sizes: [320, 640, 1280],
-      formats: ["image/webp"]
-    }
-  }),
   output: "static",
-  outDir: "./dist",
   server: {
     port: 4321,
     host: true
