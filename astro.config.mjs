d2630ee28ba24fba20356cfc3a7f11fd9a639f7e
RickCrispin <74029241@continental.edu.pe>
2026-04-28 11:57:34 -0500

    Fix Vercel deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      formats: ["image/webp"]
+    }
+  }),
   output: "static",
-  outDir: "./docs",
+  outDir: "./dist",
   server: {
     port: 4321,
     host: true
