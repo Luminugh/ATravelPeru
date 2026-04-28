b4801dbdf525d84115077ada0170ac865cddd1c9
RickCrispin <74029241@continental.edu.pe>
2026-04-28 13:08:44 -0500

    Add Vercel static adapter for on-demand rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
+  adapter: vercel(),
   output: "static",
   server: {
     port: 4321,
